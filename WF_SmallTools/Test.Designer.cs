f12ec339538c6b1490602f6a0bc49394cb96ee5d
milivoyevich <milivoyevich@gmail.com>
2015-09-13 12:16:23 +0200

    pp

--- WF_SmallTools/Test.Designer.cs
+++ WF_SmallTools/Test.Designer.cs
@@ -42,7 +42,7 @@
             this.msGlavni.BackColor = System.Drawing.Color.Transparent;
             this.msGlavni.Location = new System.Drawing.Point(0, 0);
             this.msGlavni.Name = "msGlavni";
-            this.msGlavni.Size = new System.Drawing.Size(653, 24);
+            this.msGlavni.Size = new System.Drawing.Size(634, 24);
             this.msGlavni.TabIndex = 0;
             this.msGlavni.Text = "menuStrip1";
             // 
@@ -50,7 +50,7 @@
             // 
             this.ssGlavni.Location = new System.Drawing.Point(0, 477);
             this.ssGlavni.Name = "ssGlavni";
-            this.ssGlavni.Size = new System.Drawing.Size(653, 22);
+            this.ssGlavni.Size = new System.Drawing.Size(634, 22);
             this.ssGlavni.TabIndex = 1;
             this.ssGlavni.Text = "statusStrip1";
             // 
@@ -62,7 +62,7 @@
             this.tcExamples.Location = new System.Drawing.Point(0, 24);
             this.tcExamples.Name = "tcExamples";
             this.tcExamples.SelectedIndex = 0;
-            this.tcExamples.Size = new System.Drawing.Size(653, 453);
+            this.tcExamples.Size = new System.Drawing.Size(634, 453);
             this.tcExamples.TabIndex = 2;
             // 
             // tpProcedure
@@ -71,7 +71,7 @@
             this.tpProcedure.Location = new System.Drawing.Point(4, 22);
             this.tpProcedure.Name = "tpProcedure";
             this.tpProcedure.Padding = new System.Windows.Forms.Padding(3);
-            this.tpProcedure.Size = new System.Drawing.Size(645, 427);
+            this.tpProcedure.Size = new System.Drawing.Size(626, 427);
             this.tpProcedure.TabIndex = 0;
             this.tpProcedure.Text = "Ускладиштене процедуре";
             // 
@@ -90,7 +90,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSeaGreen;
-            this.ClientSize = new System.Drawing.Size(653, 499);
+            this.ClientSize = new System.Drawing.Size(634, 499);
             this.Controls.Add(this.tcExamples);
             this.Controls.Add(this.ssGlavni);
             this.Controls.Add(this.msGlavni);
